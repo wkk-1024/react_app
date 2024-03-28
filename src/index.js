65f24cfcd2f9142c35485306e8bdc353a19e5f62
ikun <wangkun@zhiqicai.com>
2024-03-28 15:28:06 +0800

    设置移动端尺寸

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 适配移动端
+document.documentElement.style.fontSize = 100 / 750 + 'vm'
+
 root.render(
   <React.StrictMode>
     <App />
