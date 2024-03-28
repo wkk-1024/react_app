35b228a339d3e3d2610a2588c4494eaf663d97a6
ikun <wangkun@zhiqicai.com>
2024-03-28 14:20:59 +0800

    设置样式的方式

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import classes from './App.module.css'
+
 
 const App = (props) => {
+
+  const [showBgColor, setShowBgColor] = React.useState(false);
+
+  const showBgColorHandler = () => {
+    setShowBgColor(!showBgColor)
+  }
   return (
     <div>
-      apppp
+      <h1 className={`${classes.h1} ${showBgColor? classes.bgColor : ''}`}>Hello, world!</h1>
+      <button onClick={showBgColorHandler}>切换背景颜色</button>
     </div>
   );
 };
